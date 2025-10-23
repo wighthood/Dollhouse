6b98fb6f5cfa07d69e40c118b5a4593bd948abbf
wighthood <60099411+wighthood@users.noreply.github.com>
2025-10-23 16:56:49 +0200

    normally made a button to start and get ready

--- Assets/Script/PlayerSpawner.cs
+++ Assets/Script/PlayerSpawner.cs
@@ -1,17 +1,61 @@
+using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartGame : NetworkBehaviour
 {
     [SerializeField]
     private GameObject playerPrefab;
-    public void SpawnPlayer()
+    [SerializeField]
+    private Button button;
+
+    private Dictionary<ulong, bool> _players = new();
+    
+    private void Start()
     {
-        if (!IsServer)
+        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnected;
+        if (IsHost)
         {
-            return;
+            button.onClick.AddListener(SpawnPlayer);
+            button.GetComponent<TextMeshProUGUI>().text = "Start Game";
+            button.interactable = false;
         }
+        else
+        {
+            button.onClick.AddListener(IsReadyServerRpc);
+            button.GetComponent<TextMeshProUGUI>().text = "Ready";
+        }
+    }
+
+    private void OnClientConnected(ulong obj)
+    {
+        _players.Add(obj, false);
+    }
+    
+    private void OnClientDisconnected(ulong obj)
+    {
+        _players.Remove(obj);
+    }
+
+    [Rpc(SendTo.Server)]
+    private void IsReadyServerRpc()
+    {
+        _players[NetworkManager.Singleton.LocalClientId] = true;
+        foreach (var player in _players)
+            if (!player.Value)
+            {
+                button.interactable = false;
+                return;
+            }
+        button.interactable = true;
+    }
+    
+    private void SpawnPlayer()
+    {
         foreach (var player in NetworkManager.Singleton.ConnectedClientsList)
         {
             GameObject newPlayer=Instantiate(playerPrefab, transform.position, Quaternion.identity);
