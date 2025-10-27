f7121790e268113e30599878efd440bce865eefe
wighthood <60099411+wighthood@users.noreply.github.com>
2025-10-27 14:56:43 +0100

    fixed host now disconnect everyone when leaving

--- Assets/Script/LobbyButton.cs
+++ Assets/Script/LobbyButton.cs
@@ -5,7 +5,31 @@ public class LobbyButton : NetworkBehaviour
 {
     public void Disconnect()
     {
-        NetworkManager.Singleton.Shutdown();
+        if (IsHost)
+        {
+            DisconnectServerRpc();
+        }
+        else
+        {
+            TrueDisconnect();
+        }
+    }
+
+    private void TrueDisconnect()
+    {
         SceneManager.LoadScene("MainMenu");
+        NetworkManager.Singleton.Shutdown();
+    }
+    
+    [Rpc(SendTo.Server)]
+    private void DisconnectServerRpc()
+    {
+        DisconnectClientRpc();
+    }
+    
+    [ClientRpc]
+    private void DisconnectClientRpc()
+    {
+        TrueDisconnect();
     }
 }
