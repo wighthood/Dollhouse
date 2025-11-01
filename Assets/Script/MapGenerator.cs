97a52fffcd5b10ea7ff849007fde4663c820cb13
SuperPomme2Ter <SuperPomme2Ter@gmail.com>
2025-11-01 19:26:04 +0100

    changed and adapted room prefab and added ceiling in the scene game
    fixed map generation executing twice for host

--- Assets/Script/MapGenerator.cs
+++ Assets/Script/MapGenerator.cs
@@ -193,27 +193,30 @@ public class MapGenerator : NetworkBehaviour
     [Rpc(SendTo.ClientsAndHost)]
     void CreateClientRoomsRPC(GeneratedDoorData[] data)
     {
-        //roomData = data;
-        RoomDoorsRef roomInfo;
-        for (int i = 0; i < data.Length; i++)
+        if (!IsHost)
         {
-            //always taking account the root
-            if (i > 0)
+            //roomData = data;
+            RoomDoorsRef roomInfo;
+            for (int i = 0; i < data.Length; i++)
             {
-                GameObject newRoom=Instantiate(NodeCube.gameObject, Vector3.zero, Quaternion.identity);
-                newRoom.transform.position=data[i].pos;
-                roomInfo = newRoom.GetComponent<RoomDoorsRef>();
-            }
-            else
-            {
-                roomInfo = NodeCube;
-            }
+                //always taking account the root
+                if (i > 0)
+                {
+                    GameObject newRoom=Instantiate(NodeCube.gameObject, Vector3.zero, Quaternion.identity);
+                    newRoom.transform.position=data[i].pos;
+                    roomInfo = newRoom.GetComponent<RoomDoorsRef>();
+                }
+                else
+                {
+                    roomInfo = NodeCube;
+                }
             
             
             
-            for (int j = 0; j < 4; j++)
-            {
-                roomInfo.entryGOs[j].SetActive(data[i].entriesStates[j]);
+                for (int j = 0; j < 4; j++)
+                {
+                    roomInfo.entryGOs[j].SetActive(data[i].entriesStates[j]);
+                }
             }
         }
     }
