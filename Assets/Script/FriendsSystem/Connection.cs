c64bf424d5740bd5456cf37ae44681e737e9452e
wighthood <60099411+wighthood@users.noreply.github.com>
2026-04-17 14:34:31 +0200

    added  UI for message answer

--- Assets/Script/FriendsSystem/Connection.cs
+++ Assets/Script/FriendsSystem/Connection.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Collections;
-using Newtonsoft.Json;
+using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Serialization;
 
-[System.Serializable]
+[Serializable]
 public class user
 {
     public string username;
@@ -36,11 +38,24 @@ public class Connection : MonoBehaviour
     [SerializeField] private GameObject buttoncreate;
     [SerializeField] private GameObject textPasswordVerif;
     
+    [Header("return message")]
+    [SerializeField] private GameObject answerPanel;
+    [SerializeField] private TMP_Text textReturn;
+    
 
     public void CreateAccount()
     {
         if (username.text == "" || password.text == "" || passwordVerif.text != password.text)
         {
+            answerPanel.SetActive(true);
+            if (username.text == "" || password.text == "" || passwordVerif.text =="")
+            {
+                textReturn.text = "please fill all the fields";
+            }
+            if (passwordVerif.text != password.text)
+            {
+                textReturn.text = "password do not match. try again";
+            }
             cleartext();
             return;
         }
@@ -57,12 +72,17 @@ public class Connection : MonoBehaviour
     private void AccountCreationResult(string result)
     {
         cleartext();
+        answerPanel.SetActive(true);
         if (result != "User created")
         {
+            
             Debug.Log(result);
+            if (result == "{\"error\":\"ERROR: duplicate key value violates unique constraint \"users_username_key\"\n  Detail: Key (username)=(Wighthood) already exists.\"}")
+                result = "username is taken";
+            textReturn.text = result;
             return;
         }
-        
+        textReturn.text = "Account created";
         passwordVerif.gameObject.SetActive(false);
         textPasswordVerif.SetActive(false);
         buttonConfirm.SetActive(false);
@@ -74,7 +94,7 @@ public class Connection : MonoBehaviour
     {
         user newUser = new user(username.text, password.text);
         string json = JsonUtility.ToJson(newUser);
-        byte [] data = System.Text.Encoding.UTF8.GetBytes(json);
+        byte [] data = Encoding.UTF8.GetBytes(json);
         UnityWebRequest request = new UnityWebRequest(URL + "/set/users","POST");
         request.uploadHandler = new UploadHandlerRaw(data);
         request.downloadHandler = new DownloadHandlerBuffer();
@@ -82,6 +102,38 @@ public class Connection : MonoBehaviour
         
         yield return request.SendWebRequest();
         AccountCreationResult(request.downloadHandler.text);
-        Debug.Log(request.downloadHandler.text);
+    }
+
+    public void Connect()
+    {
+        if (username.text == "" || password.text == "")
+        {
+            answerPanel.SetActive(true);
+            textReturn.text = "please fill all the fields";
+            cleartext();
+            return;
+        }
+        StartCoroutine(ConnectCoroutine());
+    }
+
+    IEnumerator ConnectCoroutine()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(URL + "/get/users/" + username.text);
+        
+        
+        yield return request.SendWebRequest();
+        ConnectionResult(request.downloadHandler.text);
+    }
+    
+    private void ConnectionResult(string result)
+    {
+        if (result != "success")
+        {
+            answerPanel.SetActive(true);
+            textReturn.text = "invalid username or password";
+            cleartext();
+            return;
+        }
+        Debug.Log(result);
     }
 }
